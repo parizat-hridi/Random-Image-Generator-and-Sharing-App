a42b19133d71212314cdf5d52337e7901fa017fc
Parizat Binta Kabir <43074604+parizat-hridi@users.noreply.github.com>
2021-07-09 19:44:26 +0600

    Update Download.js

--- src/Components/Download.js
+++ src/Components/Download.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Figure from 'react-bootstrap/esm/Figure';
+import FigureImage from 'react-bootstrap/esm/FigureImage';
 
 export const Download = () => {
   let bgImage = {
-    backgroundImage: 'URL("https://cdn.wallpapersafari.com/23/67/04HRGZ.jpg")',
+    backgroundImage: 'URL("https://wallpaperaccess.com/full/1287373.jpg")',
+    height: "85vh",
+
 
   }
   let downloadButton = {
-    backgroundColor: "black", /* Green */
+    backgroundColor: "black", 
     border: "none",
     color: "white",
     padding: "15px 32px",
-    marginLeft: "20vh",
+    marginLeft: "23vh",
     textDecoration: "none",
     display: "inline-block",
     fontSize: "16px",
@@ -22,11 +26,21 @@ export const Download = () => {
     <div style={bgImage}>
 
       <h5 style={{ color: 'black', marginLeft: '4vh', paddingTop: '2vh' }} className="fw-bold">Click on the image or "Download" button to download the image:</h5><br />
-      <a href="./logo192.png" download><button style={downloadButton}>Download</button><br />
-        <img src="./logo192.png" alt="Random Image" width="400px" height="400" style={{ marginLeft: "5vh" }} />
+      <a rel="nofollow" download="" target="_blank" className="_2jBzD _1nvjo _3jtP1 _3d86A _1CBrG _1WPby xLon9 _asSA _19rc8 hhSId _1B083 _3d86A _22Rl1 _1AA_u untracked" data-test="non-sponsored-photo-download-button" title="Download photo" href="https://unsplash.com/photos/odxB5oIG_iA/download?force=true"><button style={downloadButton}>Download</button><br /><br />
+
+        <Figure>
+          <Figure.Image
+            width={500}
+            height={300}
+            alt="171x180"
+            src="https://unsplash.com/photos/odxB5oIG_iA/download?force=true"
+
+          />
+        </Figure>
       </a>
 
 
     </div>
+
   )
 }
